c3d24b7fa42e010b188b91af46ae967c151b893e
saleknia <salekniaamirhossein@gmail.com>
2025-04-17 14:52:03 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -37,18 +37,15 @@ class Mobile_netV2_loss(nn.Module):
 
         self.base = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14')
         self.base.blocks[-1] = nn.Identity()
-        self.base = nn.Sequential(
-                                    self.base.patch_embed,
-                                    self.base.blocks
-                                )
+ 
         for param in self.base.parameters():
             param.requires_grad = False
 
-        self.coarse_grain = coarse_grained_model().model.blocks[-1]
+        self.coarse_grain = coarse_grained_model()
 
-        self.fine_grain_1 = fine_grained_model(num_classes=23, superclass=1).model.blocks[-1]
-        self.fine_grain_2 = fine_grained_model(num_classes=22, superclass=2).model.blocks[-1]
-        self.fine_grain_3 = fine_grained_model(num_classes=22, superclass=3).model.blocks[-1]
+        self.fine_grain_1 = fine_grained_model(num_classes=23, superclass=1)
+        self.fine_grain_2 = fine_grained_model(num_classes=22, superclass=2)
+        self.fine_grain_3 = fine_grained_model(num_classes=22, superclass=3)
 
         self.coarse_grain_classifier = nn.Sequential(
                                     nn.Dropout(p=0.5, inplace=True),
@@ -68,6 +65,8 @@ class Mobile_netV2_loss(nn.Module):
         coarse_grain = self.coarse_grain(base)
         cgc          = self.coarse_grain_classifier(coarse_grain)
 
+        # print(cgc.shape)
+        # print(coarse_grain)
 
         fine_grain_1 = self.fine_grain_1(base) * cgc[:, 0].unsqueeze(dim=1)
         fine_grain_2 = self.fine_grain_2(base) * cgc[:, 1].unsqueeze(dim=1)
@@ -103,6 +102,11 @@ class fine_grained_model(nn.Module):
 
         for param in self.parameters():
             param.requires_grad = False
+        
+        self.model.patch_embed = nn.Identity()
+
+        for i in range(11):
+            self.model.blocks[i] = nn.Identity()
 
     def forward(self, x_in):
 
@@ -134,6 +138,11 @@ class coarse_grained_model(nn.Module):
 
         for param in self.parameters():
             param.requires_grad = False
+
+        self.model.patch_embed = nn.Identity()
+
+        for i in range(11):
+            self.model.blocks[i] = nn.Identity()
 
     def forward(self, x_in):
 
