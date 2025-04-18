053abbf8dcb82520930227d813b1753308a83ccb
saleknia <salekniaamirhossein@gmail.com>
2025-04-18 16:59:50 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -40,8 +40,8 @@ class Mobile_netV2(nn.Module):
         for param in self.model.parameters():
             param.requires_grad = False
 
-        for param in self.model.blocks[-1].parameters():
-            param.requires_grad = True
+        # for param in self.model.blocks[-1].parameters():
+        #     param.requires_grad = True
 
         self.head = nn.Sequential(
                                     nn.Dropout(p=0.5, inplace=True),
