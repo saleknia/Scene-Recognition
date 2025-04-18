328a8073398b6265dbcea85994dde914b6f65e19
saleknia <salekniaamirhossein@gmail.com>
2025-04-18 16:22:58 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -35,7 +35,7 @@ class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=67, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
-        self.model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14')
+        self.model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14')
 
         for param in self.model.parameters():
             param.requires_grad = False
@@ -45,7 +45,7 @@ class Mobile_netV2(nn.Module):
 
         self.head = nn.Sequential(
                                     nn.Dropout(p=0.5, inplace=True),
-                                    nn.Linear(in_features=1024, out_features=num_classes, bias=True),
+                                    nn.Linear(in_features=768, out_features=num_classes, bias=True),
                                 )
 
     def forward(self, x_in):
