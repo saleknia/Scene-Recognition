9729e15f3cb15b808679e765e9bad75b9ec090cd
saleknia <salekniaamirhossein@gmail.com>
2025-08-08 18:52:41 +0000

    Commit Process

--- trainer.py
+++ trainer.py
@@ -12,10 +12,67 @@ from valid import valid_func
 from torcheval.metrics import MulticlassAccuracy
 from torch.nn.modules.loss import CrossEntropyLoss
 # labels = torch.load('/content/Scene-Recognition/labels.pt').cuda()
+
+class RKD(nn.Module):
+	'''
+	Relational Knowledge Distillation
+	https://arxiv.org/pdf/1904.05068.pdf
+	'''
+	def __init__(self, w_dist, w_angle):
+		super(RKD, self).__init__()
+
+		self.w_dist  = 50
+		self.w_angle = 100
+
+	def forward(self, feat_s, feat_t):
+		loss = (self.w_dist * self.rkd_dist(feat_s, feat_t) + self.w_angle * self.rkd_angle(feat_s, feat_t)) / 2.0
+		return loss
+
+	def rkd_dist(self, feat_s, feat_t):
+		feat_t_dist = self.pdist(feat_t, squared=False)
+		mean_feat_t_dist = feat_t_dist[feat_t_dist>0].mean()
+		feat_t_dist = feat_t_dist / mean_feat_t_dist
+
+		feat_s_dist = self.pdist(feat_s, squared=False)
+		mean_feat_s_dist = feat_s_dist[feat_s_dist>0].mean()
+		feat_s_dist = feat_s_dist / mean_feat_s_dist
+
+		loss = F.smooth_l1_loss(feat_s_dist, feat_t_dist)
+
+		return loss
+
+	def rkd_angle(self, feat_s, feat_t):
+		# N x C --> N x N x C
+		feat_t_vd = (feat_t.unsqueeze(0) - feat_t.unsqueeze(1))
+		norm_feat_t_vd = F.normalize(feat_t_vd, p=2, dim=2)
+		feat_t_angle = torch.bmm(norm_feat_t_vd, norm_feat_t_vd.transpose(1, 2)).view(-1)
+
+		feat_s_vd = (feat_s.unsqueeze(0) - feat_s.unsqueeze(1))
+		norm_feat_s_vd = F.normalize(feat_s_vd, p=2, dim=2)
+		feat_s_angle = torch.bmm(norm_feat_s_vd, norm_feat_s_vd.transpose(1, 2)).view(-1)
+
+		loss = F.smooth_l1_loss(feat_s_angle, feat_t_angle)
+
+		return loss
+
+	def pdist(self, feat, squared=False, eps=1e-12):
+		feat_square = feat.pow(2).sum(dim=1)
+		feat_prod   = torch.mm(feat, feat.t())
+		feat_dist   = (feat_square.unsqueeze(0) + feat_square.unsqueeze(1) - 2 * feat_prod).clamp(min=eps)
+
+		if not squared:
+			feat_dist = feat_dist.sqrt()
+
+		feat_dist = feat_dist.clone()
+		feat_dist[range(len(feat)), range(len(feat))] = 0
+
+		return feat_dist
+
 mapping = [0, 1, 0, 1, 0, 2, 2, 1, 0, 1, 0, 2, 0, 2, 0, 1, 1, 2, 0, 0, 1, 2,
        1, 0, 0, 1, 2, 1, 1, 1, 2, 2, 2, 0, 0, 1, 2, 2, 2, 0, 1, 1, 0, 2,
        0, 2, 0, 1, 2, 2, 2, 1, 0, 2, 0, 0, 1, 0, 2, 0, 1, 0, 2, 1, 2, 1,
        1]
+
 warnings.filterwarnings("ignore")
 
 def trainer_func(epoch_num,model,dataloader,optimizer,device,ckpt,num_class,lr_scheduler,logger):
@@ -90,57 +147,3 @@ def trainer_func(epoch_num,model,dataloader,optimizer,device,ckpt,num_class,lr_s
     if ckpt is not None:
        ckpt.save_best(acc=Acc, epoch=epoch_num, net=model)
 
-class RKD(nn.Module):
-	'''
-	Relational Knowledge Distillation
-	https://arxiv.org/pdf/1904.05068.pdf
-	'''
-	def __init__(self, w_dist, w_angle):
-		super(RKD, self).__init__()
-
-		self.w_dist  = 50
-		self.w_angle = 100
-
-	def forward(self, feat_s, feat_t):
-		loss = (self.w_dist * self.rkd_dist(feat_s, feat_t) + self.w_angle * self.rkd_angle(feat_s, feat_t)) / 2.0
-		return loss
-
-	def rkd_dist(self, feat_s, feat_t):
-		feat_t_dist = self.pdist(feat_t, squared=False)
-		mean_feat_t_dist = feat_t_dist[feat_t_dist>0].mean()
-		feat_t_dist = feat_t_dist / mean_feat_t_dist
-
-		feat_s_dist = self.pdist(feat_s, squared=False)
-		mean_feat_s_dist = feat_s_dist[feat_s_dist>0].mean()
-		feat_s_dist = feat_s_dist / mean_feat_s_dist
-
-		loss = F.smooth_l1_loss(feat_s_dist, feat_t_dist)
-
-		return loss
-
-	def rkd_angle(self, feat_s, feat_t):
-		# N x C --> N x N x C
-		feat_t_vd = (feat_t.unsqueeze(0) - feat_t.unsqueeze(1))
-		norm_feat_t_vd = F.normalize(feat_t_vd, p=2, dim=2)
-		feat_t_angle = torch.bmm(norm_feat_t_vd, norm_feat_t_vd.transpose(1, 2)).view(-1)
-
-		feat_s_vd = (feat_s.unsqueeze(0) - feat_s.unsqueeze(1))
-		norm_feat_s_vd = F.normalize(feat_s_vd, p=2, dim=2)
-		feat_s_angle = torch.bmm(norm_feat_s_vd, norm_feat_s_vd.transpose(1, 2)).view(-1)
-
-		loss = F.smooth_l1_loss(feat_s_angle, feat_t_angle)
-
-		return loss
-
-	def pdist(self, feat, squared=False, eps=1e-12):
-		feat_square = feat.pow(2).sum(dim=1)
-		feat_prod   = torch.mm(feat, feat.t())
-		feat_dist   = (feat_square.unsqueeze(0) + feat_square.unsqueeze(1) - 2 * feat_prod).clamp(min=eps)
-
-		if not squared:
-			feat_dist = feat_dist.sqrt()
-
-		feat_dist = feat_dist.clone()
-		feat_dist[range(len(feat)), range(len(feat))] = 0
-
-		return feat_dist
