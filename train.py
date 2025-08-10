05e63672e4e8ab5db8b49198fa0a0503db913294
saleknia <salekniaamirhossein@gmail.com>
2025-08-10 09:59:01 +0000

    Commit Process

--- train.py
+++ train.py
@@ -51,19 +51,23 @@ def main(args):
 
         transform_train = transforms.Compose([
             # transforms.Resize((224, 224)),
-            transforms.RandomResizedCrop(size=224, scale=(0.2, 1.0)),
+            # transforms.RandomResizedCrop(size=224, scale=(0.2, 1.0)),
+            # transforms.RandomHorizontalFlip(),
+            # transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            # transforms.RandomGrayscale(p=0.2),
+            transforms.RandomSizedCrop(224),
             transforms.RandomHorizontalFlip(),
-            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-            transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
             # transforms.RandomErasing(p=1.0),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
         transform_valid = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            # transforms.Resize((224, 224)),
+            # transforms.ToTensor(),
+            transforms.Scale(256),
+            transforms.CenterCrop(224),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
         transform_test = transforms.Compose([
