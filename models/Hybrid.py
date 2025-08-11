8803391addefaf4e19648111a6aaf15361407e8b
saleknia <salekniaamirhossein@gmail.com>
2025-08-11 15:35:26 +0000

    Commit Process

--- models/Hybrid.py
+++ models/Hybrid.py
@@ -25,44 +25,80 @@ from torchvision.transforms import FiveCrop, Lambda
 
 from transformers import AutoModelForImageClassification
 
+from .ConvNext import ConvNext
+from .ResNet import ResNet
+
+scene      = ResNet().cuda()
+checkpoint = torch.load('/content/drive/MyDrive/checkpoint/scene.pth', map_location='cuda')
+scene.load_state_dict(checkpoint['net'])
+
+obj        = ConvNext().cuda()
+checkpoint = torch.load('/content/drive/MyDrive/checkpoint/obj.pth', map_location='cuda')
+obj.load_state_dict(checkpoint['net'])
+
+
 class Hybrid(nn.Module):
     def __init__(self, num_classes=67, pretrained=True):
         super(Hybrid, self).__init__()
 
-        self.scene = models.__dict__['resnet50'](num_classes=365).cuda()
-        checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cuda')
-        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
-        self.scene.load_state_dict(state_dict)
+        # self.scene = models.__dict__['resnet50'](num_classes=365).cuda()
+        # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cuda')
+        # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        # self.scene.load_state_dict(state_dict)
 
-        for param in self.scene.parameters():
-            param.requires_grad = False
+        # for param in self.scene.parameters():
+        #     param.requires_grad = False
 
-        for param in self.scene.layer4[-1].parameters():
-            param.requires_grad = True
+        # for param in self.scene.layer4[-1].parameters():
+        #     param.requires_grad = True
 
-        self.scene.fc = nn.Sequential(
-                                    nn.Dropout(p=0.5, inplace=True),
-                                    nn.Linear(in_features=2048, out_features=256, bias=True)
-                                )
+        # self.scene.fc = nn.Sequential(
+        #                             nn.Dropout(p=0.5, inplace=True),
+        #                             nn.Linear(in_features=2048, out_features=256, bias=True)
+        #                         )
+        # ###########################################
+        # ###########################################
+        # self.obj = timm.create_model("timm/convnext_tiny.fb_in22k", pretrained=True)
+
+        # for param in self.obj.parameters():
+        #     param.requires_grad = False
+
+        # for param in self.obj.stages[-1].blocks[-1].parameters():
+        #     param.requires_grad = True
+
+        # self.obj.head.fc = nn.Sequential(
+        #                             nn.Dropout(p=0.5, inplace=True),
+        #                             nn.Linear(in_features=768, out_features=256, bias=True),
+        #                         )
+
+        # for param in self.obj.head.parameters():
+        #     param.requires_grad = True
         ###########################################
         ###########################################
-        self.obj = timm.create_model("timm/convnext_tiny.fb_in22k", pretrained=True)
+    
+        self.scene = scene
+        self.obj   = obj
 
         for param in self.obj.parameters():
             param.requires_grad = False
 
-        for param in self.obj.stages[-1].blocks[-1].parameters():
+        for param in self.obj.head.parameters():
             param.requires_grad = True
+
+        for param in self.scene.parameters():
+            param.requires_grad = False
 
         self.obj.head.fc = nn.Sequential(
                                     nn.Dropout(p=0.5, inplace=True),
                                     nn.Linear(in_features=768, out_features=256, bias=True),
                                 )
 
-        for param in self.obj.head.parameters():
-            param.requires_grad = True
-        ###########################################
-        ###########################################
+
+        self.scene.fc = nn.Sequential(
+                                    nn.Dropout(p=0.5, inplace=True),
+                                    nn.Linear(in_features=2048, out_features=256, bias=True)
+                                )
+
         self.head = nn.Sequential(
                             nn.Dropout(p=0.5, inplace=True),
                             nn.Linear(in_features=512, out_features=num_classes, bias=True),
@@ -73,7 +109,7 @@ class Hybrid(nn.Module):
         obj   = self.obj(x_in)
         scene = self.scene(x_in)
 
-        features = obj + scene
+        features = torch.cat([obj, scene], dim=1)
 
         x = self.head(features)
         
