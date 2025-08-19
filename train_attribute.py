53cd7947c71a22b347c6c987b7db7d1fc0683d72
saleknia <salekniaamirhossein@gmail.com>
2025-08-19 12:40:57 +0000

    Commit Process

--- train_attribute.py
+++ train_attribute.py
@@ -60,7 +60,8 @@ def main(args):
         image_paths = [str(p) for p in image_paths]
 
         transform_train = transforms.Compose([
-            transforms.Resize((224, 224)),
+            # transforms.Resize((224, 224)),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
