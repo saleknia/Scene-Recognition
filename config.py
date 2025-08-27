d16dae59bcc666c386d2d25d99bbef849d3fa1e4
saleknia <salekniaamirhossein@gmail.com>
2025-08-27 10:37:51 +0000

    Commit Process

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ if log:
 LEARNING_RATE = 0.01
 DEVICE        = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE    = 64
-NUM_EPOCHS    = 20
+NUM_EPOCHS    = 30
 NUM_WORKERS   = 4
 IMAGE_HEIGHT  = 224
 IMAGE_WIDTH   = 224
