8c78c7b47d39cd2156d8e37b674f2073cb886a44
saleknia <salekniaamirhossein@gmail.com>
2026-05-25 07:36:36 +0000

    Commit Process

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ def main(args):
         train_loader = torch.utils.data.DataLoader(trainset, batch_size = BATCH_SIZE, shuffle=True , num_workers=NUM_WORKERS)
         test_loader  = torch.utils.data.DataLoader(testset , batch_size = 1         , shuffle=False, num_workers=NUM_WORKERS)
 
-        NUM_CLASS = len(trainset.classes)
+        NUM_CLASS = 40
 
         data_loader  = {'train':train_loader,'valid':test_loader, 'test':test_loader}
         
